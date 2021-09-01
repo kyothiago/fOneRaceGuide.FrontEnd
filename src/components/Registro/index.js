05fea748a549a84342fb379c449b8f28ce5a21c4
rebecanonato89 <rebecanonato89@gmail.com>
2021-09-01 20:50:55 -0300

    feat: created user

--- src/components/Registro/index.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-
-import './styles.css';
-
-import logo from "./images/f1-new-logo.svg"
-
-export function login(){
-    const [usuarioId, ]
-}
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,79 +1,92 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { FiArrowLeft } from 'react-icons/fi';
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { FiArrowLeft } from "react-icons/fi";
 
-import './styles.css';
-import f1Logo from "../../assets/f1-new-logo.png";
+import api from "../../services/api";
 
-export default function Register(){
+import "./styles.css";
 
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [celular, setCelular] = useState('');
-    const [login, setLogin] = useState('');
-    const [senha, setSenha] = useState('');
+export default function Register() {
+  const history = useHistory();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [celular, setCelular] = useState("");
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
 
-    async function handleRegister(e){
-        e.preventDefault();
+  async function handleRegister(e) {
+    e.preventDefault();
 
-        const data = {
-            name,
-            email,
-            celular,
-            login,
-            senha
-        };
+    const data = {
+      name,
+      email,
+      celular,
+      login,
+      password,
+    };
+
+    try {
+      const response = await api.post("/user/add", data);
+      alert("salvo com sucesso", response);
+
+      history.push("/");
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    return (
-        <div className="register-container">
-            <div className="content">
-                <section>
-                    <h1>Cadastro</h1>
-                    <p>Faça seu cadastro, entre na plataforma e acompanhe 
-                        todos os detalhes dos circuitos da Formula 1. 
-                    </p>
+  return (
+    <div className="register-container">
+      <div className="content">
+        <section>
+          <h1>Cadastro</h1>
+          <p>
+            Faça seu cadastro, entre na plataforma e acompanhe todos os detalhes
+            dos circuitos da Formula 1.
+          </p>
 
-                    <Link className="back-link" to="/">
-                        <FiArrowLeft size={16} color="#FF1801"/>
-                        Voltar
-                    </Link>
-                </section>
+          <Link className="back-link" to="/">
+            <FiArrowLeft size={16} color="#FF1801" />
+            Voltar
+          </Link>
+        </section>
 
-                <form onSubmit={ handleRegister }>
-                <input 
-                        placeholder="Login" 
-                        name={login}
-                        onChange={ e => setLogin(e.target.value)}
-                    />
-                    <input 
-                        placeholder="Nome" 
-                        name={name}
-                        onChange={ e => setName(e.target.value)}
-                    />
-                    <input 
-                        type="email" 
-                        placeholder="E-mail" 
-                        name={email}
-                        onChange={ e => setEmail(e.target.value)}
-                        />
-                    <input 
-                        placeholder="Celular"
-                        name={celular}
-                        onChange={ e => setCelular(e.target.value)}
-                    />
+        <form onSubmit={handleRegister}>
+          <input
+            placeholder="Login"
+            name={login}
+            onChange={(e) => setLogin(e.target.value)}
+          />
+          <input
+            placeholder="Nome"
+            name={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="E-mail"
+            name={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            placeholder="Celular"
+            name={celular}
+            onChange={(e) => setCelular(e.target.value)}
+          />
 
-                    <div className="input-group">
-                        <input 
-                            placeholder="Senha"
-                            name={senha}
-                            type="password"
-                            onChange={ e => setSenha(e.target.value)}
-                        />
-                    </div>
-                    <button className="button" type="submit">Cadastrar</button>
-                </form>
-            </div>
-        </div>
-    );
-};
+          <div className="input-group">
+            <input
+              placeholder="Senha"
+              name={password}
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button className="button" type="submit">
+            Cadastrar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
