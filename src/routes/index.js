63129b9f4bdd66a7961b0f638df97177caaf6a33
rebecanonato89 <rebecanonato89@gmail.com>
2021-09-05 11:18:12 -0300

    feat: authenticated routes

---
+++ src/routes/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter, Switch } from "react-router-dom";
+import Route from "./Route";
+import Home from "../pages/Home";
+
+import Logon from "../pages/Logon";
+import Register from "../pages/Register";
+import UploadCircuito from "../pages/Upload";
+import RegisterCircuit from "../pages/NewCircuit";
+import UpdateProfile from "../pages/UpdateProfile";
+import Relatorio from "../pages/Relatorio";
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Logon} />
+        <Route path="/register" component={Register} />
+        <Route path="/home" component={Home} isPrivate />
+        <Route path="/newCircuit" component={RegisterCircuit} isPrivate />
+        <Route path="/circuitUpload" component={UploadCircuito} isPrivate />
+        <Route path="/updateProfile" component={UpdateProfile} isPrivate />
+        <Route path="/relatorio" component={Relatorio} isPrivate />
+      </Switch>
+    </BrowserRouter>
+  );
+}
